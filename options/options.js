35f541fcd7dd3d6f20c57bbff9c2736009d51021
Crowdbotics <team@crowdbotics.com>
2024-03-14 09:48:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-voice-47038",
+  url: "https://spring-voice-47038.botics.co",
+  api: "https://spring-voice-47038.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
